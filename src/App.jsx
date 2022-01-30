a6eda70827909bce3f89505a2cdac4247e54ecf7
Emon-Adrian <matovuemon20@gmail.com>
2022-01-30 13:56:36 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DataTableExample from './DataTableExample';
+
+const App = () => {
+  return <div>
+    <DataTableExample/>
+  </div>;
+};
+
+export default App;
